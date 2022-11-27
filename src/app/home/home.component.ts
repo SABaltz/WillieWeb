d40618aad130dd52a8255885b41d468fdf2886f7
sam <samuelabaltz@gmail.com>
2022-11-27 10:20:54 -0700

    setup broken drum

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 import {Subscription} from "rxjs";
 import {DataService} from "../data.service";
 
@@ -9,10 +9,13 @@ import {DataService} from "../data.service";
 })
 export class HomeComponent implements OnInit {
 
-  count:number;
+  audio: any;
+
+  count: number;
   subscription: Subscription;
 
-  constructor(private data: DataService) { }
+  constructor(private data: DataService) {
+  }
 
   ngOnInit() {
     this.subscription = this.data.currentNumber.subscribe(number => this.count = number)
@@ -23,8 +26,27 @@ export class HomeComponent implements OnInit {
   }
 
   newNumber() {
-    this.count ++
+    this.count++
     this.data.updateCount(this.count)
   }
 
+  playAudio() {
+    let audio = new Audio();
+    audio.src = "../assets/Snare1a.mp3";
+    if (this.count == 1) {
+      audio.src = "../assets/Snare1a.mp3";
+    } else if (this.count == 2) {
+      audio.src = "../assets/Snare2a.mp3";
+    } else if (this.count == 3) {
+      audio.src = "../assets/Snare3a.mp3";
+    } else if (this.count == 4) {
+      audio.src = "../assets/Snare4a.mp3";
+    } else if (this.count == 5) {
+      audio.src = "../assets/Snare6a.mp3";
+    } else if (this.count == 6) {
+      audio.src = "../assets/Breaking.mp3";
+    }
+    audio.load();
+    audio.play();
+  }
 }
